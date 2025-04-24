29bcedb4d926f1cb7361b0dd782ce744b02eff82
Zaiomeny <zaiomenyfanampimahafaly@gmail.com>
2025-04-24 15:58:41 +0300

    Update the application routes, apply some changes to the dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ActeurController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjetController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -19,6 +22,19 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Projet
+    Route::get('/projects',[ProjetController::class,'index'])->name('projets.index');
+    Route::post('/projects',[ProjetController::class,'store'])->name('projets.store');
+    Route::patch('/project/{projet_id}',[ProjetController::class,'update'])->name('projets.update');
+    Route::delete('/project/{projet_id}',[ProjetController::class,'destroy'])->name('projet.destroy');
+    //Acteur
+    Route::get('/project/{projet_id}/acteurs',[ActeurController::class,'index'])->name('acteurs.index');    
+    Route::post('/project/{projet_id}/acteurs',[ActeurController::class,'store'])->name('acteurs.store');
+    Route::post('/acteur/{acteur_id}',[ActeurController::class,'destroy'])->name('acteur.destroy');
+    Route::patch('/acteur/{acteur_id}',[ActeurController::class,'update'])->name('acteurs.update');
+    //Search
+    Route::get('/search/{keyword}',SearchController::class)->name('search');
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
